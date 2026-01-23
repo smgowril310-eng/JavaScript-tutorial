48701851a21175b1c02d4f947a3462491dadd802
Gowri <smgowril310@gmail.com>
2026-01-23 14:36:24 +0000

    for loop are committed

---
+++ Control flow statement/Exercise2.js
@@ -0,0 +1,7 @@
+for(count=1;count<=10;count=count+1)
+{
+    if(count%2==0)
+    {
+        console.log(count);
+    }
+}
---
+++ Control flow statement/Exercise3.js
@@ -0,0 +1,4 @@
+for(num=1;num<=10;num=num+1)
+{
+    console.log(num+"x2="+(num*2))
+}
