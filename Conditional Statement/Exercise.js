e79f7a856f0cc550378c3b2f6793f9f932051cc3
Gowri <smgowril310@gmail.com>
2026-01-23 13:51:10 +0000

    Conditional statements are committed

---
+++ Conditional Statement/Exercise.js
@@ -0,0 +1,13 @@
+var Color= "red";
+if(Color=="red")
+{
+    console.log("Stop");
+}
+if(Color=="yellow")
+{
+    console.log("Get ready");
+}
+if(Color=="green")
+{
+    console.log("Go");
+}
---
+++ Conditional Statement/Exercise1.js
@@ -0,0 +1,13 @@
+var score=45
+if(score<50)
+{
+    console.log("you need to improve")
+}
+if(score>50&&score<70)
+{
+    console.log("good job!")
+}
+if(score>70)
+{
+    console.log("Excellent performance!")
+}
