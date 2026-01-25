bdc4ec7672d794438fd6dbea80d2eba947649d19
Gowri <smgowril310@gmail.com>
2026-01-25 06:09:35 +0000

    Random number are committed

---
+++ Ramdom Number/Math.random.js
@@ -0,0 +1,14 @@
+var a=Math.random() //0-9 only
+console.log(a)
+
+var b=Math.random()
+console.log(b*10)
+
+var c=Math.floor(1.33)
+console.log(c)
+
+var d=Math.random()
+console.log(Math.floor(d*10))
+
+var e=Math.random() //1-10
+console.log(Math.floor(e*10)+1)
