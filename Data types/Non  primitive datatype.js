1677e1a8d077f685aba2ce9b8514bf5c4f674c9c
Gowri <smgowril310@gmail.com>
2026-01-26 09:49:57 +0000

    Array loop

--- Data types/Non  primitive datatype.js
+++ Data types/Non  primitive datatype.js
@@ -1,2 +1,11 @@
 Object
-Array
+Array 
+var fruit=["Apple","Orange","Banana","Grape"]
+console.log(fruit[1])
+
+//loop
+var fruit=["Apple","Orange","Grape","Banana"]
+for(count=0;count<fruit.length;count=count+1)
+{
+    console.log(fruit[count])
+}
